6afdc15ca2dd27ba81041cc0197058d56db916d1
rucpata <rucinska.patrycja@gmail.com>
2021-06-21 19:26:26 +0200

    story

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './containers/Header/Header';
 import Intro from './containers/Intro/Intro';
+import Story from './containers/Story/Story';
 import InfoList from './containers/InfoList/InfoList';
 import Form from './containers/Form/Form';
 import Footer from './containers/Footer/Footer';
@@ -24,7 +25,7 @@ const App = () => {
       </Router>
          
       <Intro intro={intros.find(intro => intro.id === 1)}/>
-      <br/><br/><br/>
+      <Story/>
       {/* <InfoList/> */}
       <Intro intro={intros.find(intro => intro.id === 2)}/>
       <br/><br/><br/>
---
+++ src/containers/Story/Story.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Story.css';
+
+const Story = () => {
+
+    return(
+        <div className='story'>
+            <h2>Piracka przygoda</h2>
+            <p>Setki lat temu 4 najwspanialszych pirackich kapitanów ukryło swoje największe skarby w drewnianych skrzyniach. Zabezpieczyli je mocą 4 magicznych talizmanów. Czy uda wam się znaleźć jedną skrzynie, która zawiera najcenniejsze skarby. Czy uda wam się rozwiązać szereg zagadek, by zostać najbogatszymi ludźmi na świecie? <br/><span>Przekonajmy się.</span></p>
+        </div>
+    )
+}
+
+export default Story;
